e1796c34287ad403704b1cff58fc8e9ee28bf3f4
juliagsch <julia.gschwind@hotmail.com>
2025-04-22 11:23:03 +0100

    Create dataset with EV charging policies

---
+++ data/ev/ev_simulation.py
@@ -0,0 +1,189 @@
+# This generator does not allow for fine-grained control of the non-commuting trips, use ev_simulation_extended.py if you wish more fine grained control over the non-commuting trips.
+# each trip is outputed separately and multiple trips on the same day are not grouped together, use merge_trips.py to merge overalpping trips.
+import argparse
+import numpy as np
+import csv
+import random
+import os
+from merge_trips import process_file
+
+class ElectricVehicle:
+    def __init__(self, battery_size, max_soc, min_soc, consumption):
+        self.battery_size = battery_size
+        self.max_soc = max_soc
+        self.min_soc = min_soc
+        self.consumption = consumption
+
+    def compute_SOC_arr(self, dist_km):
+        used_kwh = (dist_km * self.consumption) / 1000  
+        soc_change = used_kwh / self.battery_size
+        return max(self.max_soc * self.battery_size - soc_change, self.min_soc * self.battery_size)
+
+def validate_input(args):
+    if not (0 <= args.max_soc <= 1 and 0 <= args.min_soc <= 1):
+        raise ValueError("SOC values must be between 0 and 1")
+    if args.max_soc < args.min_soc:
+        raise ValueError("Max SOC must be greater than Min SOC")
+    if args.ev_battery <= 0:
+        raise ValueError("EV battery size must be a positive number")
+    if args.consumption <= 0:
+        raise ValueError("EV consumption must be a positive number")
+    if not (0 <= args.C_dept < 24 and 0 <= args.C_arr < 24):
+        raise ValueError("Departure and arrival times must be within 24-hour range")
+    if args.C_dept >= args.C_arr:
+        raise ValueError("Departure time must be before arrival time")
+    for day in [args.wfh_monday, args.wfh_tuesday, args.wfh_wednesday, args.wfh_thursday, args.wfh_friday]:
+        if day not in [0, 1]:
+            raise ValueError("WFH day inputs must be either 0 or 1")
+    if args.days <= 0:
+        raise ValueError("Number of days must be a positive number")
+    if args.C_dist < 0 :
+        raise ValueError("Commute distance must be non-negative")
+    if args.N_nc < 0:
+        raise ValueError("Number of weekly non-commuting trips must be non-negative")
+
+
+def generate_trip_data(args, ev):
+    trip_data = []
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    wfh_days = {
+        0: args.wfh_monday,
+        1: args.wfh_tuesday,
+        2: args.wfh_wednesday,
+        3: args.wfh_thursday,
+        4: args.wfh_friday
+    }
+
+    def reduce_soc(distance_km, current_soc):
+        energy_used = (distance_km * ev.consumption) / 1000  # Convert Wh to kWh
+        soc_after_trip = current_soc - energy_used
+        return max(ev.min_soc * ev.battery_size, soc_after_trip)
+    # N_nc is the number of weekly non-commuting round trips
+    average_non_commute_trips_per_day = args.N_nc / 7
+    average_holiday_weeks = args.N_hw
+    average_speed_kmh = 50
+    week_number = 0
+    holiday_weeks = random.sample([i for i in range(1,53)], average_holiday_weeks)
+    print(holiday_weeks)
+    for day in range(args.days):
+        week_day = day % 7
+        if(week_day == 0):
+            week_number += 1
+        is_wfh_day = wfh_days.get(week_day, 0) == 1
+        is_weekend = week_day in [5, 6]
+        # assume the EV is fully charged at the beginning of each day
+        current_soc = ev.max_soc * ev.battery_size
+        trips_today = []
+
+        # Add commuting trips on non-WFH weekdays
+        if not is_wfh_day and not is_weekend and not week_number in holiday_weeks:
+            commute_dist = random.uniform(args.C_dist - args.C_dist * 0.1, args.C_dist + args.C_dist * 0.1)
+            t_dep, t_arr = sample_commute_times(args)
+            soc_start = current_soc
+            soc_end = reduce_soc(commute_dist, soc_start)
+            commute_travel_time = (args.C_arr - args.C_dept) * 60  
+            trips_today.append((weekdays[week_day], format_time(t_dep), f"{soc_start:.2f}", format_time(t_arr), f"{soc_end:.2f}", f"{commute_dist:.2f}", round(commute_travel_time)))
+            current_soc = soc_end
+
+        # Non-commuting trips
+        num_non_commute_trips = np.random.poisson(average_non_commute_trips_per_day)
+        for _ in range(num_non_commute_trips):
+            t_dep = sample_non_commute_times(args)
+            non_commute_dist = args.Nc_dist
+            travel_time_hours = non_commute_dist / average_speed_kmh
+             # we assume that for non commuting trips, the EV is driving a 20% of the trip duration 
+            trip_duration = 5*travel_time_hours
+            t_arr = t_dep + trip_duration
+            if t_arr >= 24:
+                t_arr -= 24
+
+            # Non-commuting distance round trip
+            soc_start = current_soc
+            soc_end = reduce_soc(non_commute_dist, soc_start)
+            non_commute_travel_time = trip_duration * 60  
+            trips_today.append((weekdays[week_day], format_time(t_dep), f"{soc_start:.2f}", format_time(t_arr), f"{soc_end:.2f}", f"{non_commute_dist:.2f}", round(non_commute_travel_time)))
+            current_soc = soc_end
+
+        if not trips_today:
+            trips_today.append((weekdays[week_day], "No trips", "32.0", "", "32.0"))
+
+        trip_data.append((day + 1, trips_today))
+
+    return trip_data
+
+
+def format_time(time_float):
+    """Converts time in float format to HH:MM format"""
+    hours = int(time_float)
+    minutes = int((time_float - hours) * 60)
+    return f"{hours:02d}:{minutes:02d}"
+
+def sample_commute_times(args):
+    """Sample departure and arrival times for commuting, ensuring consistency"""
+    t_dep_hour = random.uniform(args.C_dept - 0.25, args.C_dept + 0.25)  # Departure time with small variance
+    t_arr_hour = random.uniform(args.C_arr - 0.25, args.C_arr + 0.25)  # Arrival time with small variance
+    
+    while t_arr_hour <= t_dep_hour:
+        t_arr_hour = random.uniform(args.C_arr - 0.25, args.C_arr + 0.25)
+
+    return t_dep_hour, t_arr_hour
+
+def sample_non_commute_times(args):
+    """Randomly sample departure time for non-commuting trips, ensuring consistency"""
+    t_dep_hour = random.uniform(8, 20)  # Assuming non-commuting trips can start between 8 AM and 8 PM
+    
+    return t_dep_hour
+
+def write_to_csv(file_name, data):
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Day", "Weekday", "Departure Time", "SOC on Departure", "Arrival Time", "SOC on Arrival", "Distance (km)", "Travel Time (min)"])
+        for day, trips in data:
+            for trip in trips:
+                writer.writerow([day, *trip])
+
+
+def run_simulation(args):
+    validate_input(args)
+    ev = ElectricVehicle(args.ev_battery, args.max_soc, args.min_soc, args.consumption)
+    return generate_trip_data(args, ev)
+
+
+def main(args):
+    validate_input(args)
+    ev = ElectricVehicle(args.ev_battery, args.max_soc, args.min_soc, args.consumption)
+    trip_data = generate_trip_data(args, ev)
+    temp_path = str(random.randint(1000, 9999))+"_temp.csv"
+    write_to_csv(temp_path, trip_data)
+    process_file(temp_path, args.output)
+
+    if os.path.exists(temp_path):
+        os.remove(temp_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Sample synthetic EV usage data.')
+
+    # Adding all the arguments from your initial code
+    parser.add_argument('--output', type=str, default='ev_usage.csv', help='Output file name')
+    parser.add_argument('--days', type=int, default=365, help='Desired number of days sampled')
+    parser.add_argument('--ev_battery', type=float, default=40, help='EV battery size in kWh')
+    parser.add_argument('--max_soc', type=float, default=0.8, help='Maximum state of charge')
+    parser.add_argument('--min_soc', type=float, default=0.2, help='Minimum state of charge')
+    parser.add_argument('--consumption', type=float, default=164, help='Consumption in Wh/km')
+    parser.add_argument('--wfh_monday', type=int, default=0, choices=[0, 1], help='WFH on Monday')
+    parser.add_argument('--wfh_tuesday', type=int, default=0, choices=[0, 1], help='WFH on Tuesday')
+    parser.add_argument('--wfh_wednesday', type=int, default=0, choices=[0, 1], help='WFH on Wednesday')
+    parser.add_argument('--wfh_thursday', type=int, default=0, choices=[0, 1], help='WFH on Thursday')
+    parser.add_argument('--wfh_friday', type=int, default=0, choices=[0, 1], help='WFH on Friday')
+    parser.add_argument('--C_dist', type=float, default=20.0, help='Typical return commute distance in km')
+    parser.add_argument('--C_dept', type=float, default=7.45, help='Departure time for commuting')
+    parser.add_argument('--C_arr', type=float, default=17.30, help='Arrival time from commuting')
+    parser.add_argument('--N_nc', type=int, default=3, help='Weekly number of non-commuting round trips')
+    parser.add_argument('--N_hw', type=int, default=6, help='Number of holiday weeks per year')
+    parser.add_argument('--Nc_dist', type=int, default=15, help='Average distance of a non-commute trip')
+
+    args = parser.parse_args()
+    try:
+        main(args)
+    except ValueError as e:
+        print(f"Input Error: {e}")
--- dataset/create_dataset.py
+++ dataset/create_dataset.py
@@ -4,17 +4,16 @@ import csv
 import time
 import multiprocessing
 import random
+from dataclasses import dataclass
 
-
-def get_solar_files():
-    return [f"./data/solar/processed_all/{f}" for f in os.listdir("./data/solar/processed_all") if f.endswith('.txt')] + [f"./data/solar/noisy/{f}" for f in os.listdir("./data/solar/noisy") if f.endswith('.txt')]
-
-
-def get_load_files():
-    files = []
-    for type in ["Detached", "EV", "NoLCT", "Semi-detached", "Terraced", "Noisy"]:
-        files = files + [f"./data/load/processed_poster/{type}/{f}" for f in os.listdir(f"./data/load/processed_poster/{type}") if f.endswith('.txt')]
-    return files
+@dataclass
+class EV:
+    num_commute_trips: int
+    num_non_commute_trips: int
+    avg_commute_distance: float
+    avg_non_commute_distance: float
+    battery_size_kwh: float
+    min_charge_kwh: float
 
 def get_files(filepath):
     try:
@@ -25,11 +24,11 @@ def get_files(filepath):
         print(f"Error: File not found at {filepath}")
         return []
 
-def process_pair_train(args):
-    round_num, idx, solar_file, load_file = args
+def process_pair(args):
+    ev_path, solar_file, load_file, op, ev, split = args
 
     try:
-        command = f"./sim_huang 1250 460 70 225 0 0.15 0.9 365 {load_file} {solar_file}"
+        command = f"./sim 1250 460 70 225 0 0.15 0.9 365 {load_file} {solar_file} 0.8 0.2 {ev.battery_size_kwh} 7.4 {op} {ev_path} {ev.min_charge_kwh}"
         result = subprocess.run(command.split(), stdout=subprocess.PIPE, text=True)
         result = result.stdout.split("\t")
         battery, solar = result[0], result[1]
@@ -40,51 +39,25 @@ def process_pair_train(args):
         with open(load_file, 'r') as file:
             load_trace = [float(line.strip()) for line in file]
 
-        line = solar_trace + load_trace + [battery, solar]
+        ev_data = [op, ev.num_commute_trips, ev.num_non_commute_trips, ev.avg_commute_distance, ev.avg_non_commute_distance, ev.battery_size_kwh, ev.min_charge_kwh]
+        line = solar_trace + load_trace + ev_data + [battery, solar]
 
-        with open("dataset_paper_train.csv", 'a', newline='') as file:
+        with open(f"dataset_{split}.csv", 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(line)
-        with open("files_processed_paper_train.csv", 'a', newline='') as file:
+        with open(f"files_processed_{split}.csv", 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow([solar_file])
             writer.writerow([load_file])
         return True
     except Exception as e:
-        print(f"Error processing {solar_file} and {load_file} in round {round_num}: {e}")
+        print(f"Error processing {solar_file} and {load_file}: {e}")
         return False
-    
-def process_pair_test(args):
-    round_num, idx, solar_file, load_file = args
 
-    try:
-        with open(solar_file, 'r') as file:
-            solar_trace = [float(line.strip()) for line in file]
-
-        command = f"./sim_huang 1250 460 70 225 0 0.15 0.9 365 {load_file} {solar_file}"
-        result = subprocess.run(command.split(), stdout=subprocess.PIPE, text=True)
-        result = result.stdout.split("\t")
-        battery, solar = result[0], result[1]
-
-        with open(load_file, 'r') as file:
-            load_trace = [float(line.strip()) for line in file]
-
-        line = solar_trace + load_trace + [battery, solar]
-
-        # Append as a new row to the CSV file
-        with open("dataset_paper_test.csv", 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(line)
-        with open("files_processed_paper_test.csv", 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([solar_file])
-            writer.writerow([load_file])
-        return True
-    except Exception as e:
-        print(f"Error processing {solar_file} and {load_file} in round {round_num}: {e}")
-        return False
 
 if __name__ == "__main__":
+    num_runs = 1
+    ev_consumption = 0.164 #kWh/km
     train_solar_filepath = "./dataset/train_solar.txt"
     test_solar_filepath = "./dataset/test_solar.txt"
     train_load_filepath = "./dataset/train_load.txt"
@@ -95,35 +68,84 @@ if __name__ == "__main__":
     train_load = get_files(train_load_filepath)
     train_solar = get_files(train_solar_filepath)
 
-    num_processes = multiprocessing.cpu_count()  # Get the number of available CPU cores
+    # Remove data that already includes EV
+    train_load = [f for f in train_load if "EV" not in f]
+    test_load = [f for f in test_load if "EV" not in f]
+
+    num_processes = 4 #multiprocessing.cpu_count()  # Get the number of available CPU cores
+    policies = ["safe_arrival", "safe_departure", "arrival_limit", "lbn_limit"]
+
     print(f"Using {num_processes} processes for parallel execution.")
+    for round_num in range(num_runs):
+        random.shuffle(train_load)
+        random.shuffle(train_solar)
 
-    # for round_num in range(3):
-    #     random.shuffle(train_load)
-    #     random.shuffle(train_solar)
+        tasks = []
+        for idx, load_file in enumerate(train_load):
 
-    #     tasks = []
-    #     for idx, solar_file in enumerate(train_solar):
-    #         load_file = train_load[idx]
-    #         tasks.append((round_num, idx, solar_file, load_file))
+            solar_file = train_solar[idx]
+            ev = EV(
+                num_commute_trips=random.randint(0,5),
+                num_non_commute_trips=random.randint(0,7),
+                avg_commute_distance=random.randint(10, 110),
+                avg_non_commute_distance=random.randint(10,20),
+                battery_size_kwh=random.randint(50,100),
+                min_charge_kwh=0.0
+            )
+            # Set min charge based on maximum expected distance
+            max_distance = max(ev.avg_commute_distance, ev.avg_non_commute_distance)
+            ev.min_charge_kwh = max_distance * ev_consumption + ev.battery_size_kwh * 0.2
 
-    #     with multiprocessing.Pool(processes=num_processes) as pool:
-    #         results = pool.map(process_pair_train, tasks)
+            ev_path = f"./data/ev/train/{idx}.csv"
+            wfh_days = random.sample([0,1,2,3,4], 5-ev.num_commute_trips)
 
-    #     print(f"Round {round_num + 1} completed.")
-    
-    for round_num in range(3):
+            schedule = [0 for _ in range(5)]
+            for wfh_day in wfh_days:
+                schedule[wfh_day] = 1
+            ev_trace = f"python ./data/ev/ev_simulation.py --output {ev_path} --days 365 --ev_battery {ev.battery_size_kwh} --max_soc 0.8 --min_soc 0.2 --consumption 164 --wfh_monday {schedule[0]} --wfh_tuesday {schedule[1]} --wfh_wednesday {schedule[2]} --wfh_thursday {schedule[3]}  --wfh_friday {schedule[4]} --C_dist {ev.avg_commute_distance} --C_dept 8.00 --C_arr 18.00 --N_nc {ev.num_non_commute_trips} --Nc_dist {ev.avg_non_commute_distance}"
+            _ = subprocess.run(ev_trace.split(), stdout=subprocess.PIPE, text=True)
+
+            for op in policies:
+                tasks.append((ev_path, solar_file, load_file, op, ev, "train"))
+
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            results = pool.map(process_pair, tasks)
+
+        print(f"Round {round_num + 1} completed.")
+
+    for round_num in range(num_runs):
         random.shuffle(test_load)
         random.shuffle(test_solar)
 
         tasks = []
-        for idx, solar_file in enumerate(test_solar):
-            load_file = test_load[idx]
-            tasks.append((round_num, idx, solar_file, load_file))
+        for idx, load_file in enumerate(test_load):
+
+            solar_file = test_solar[idx]
+            ev = EV(
+                num_commute_trips=random.randint(0,5),
+                num_non_commute_trips=random.randint(0,7),
+                avg_commute_distance=random.randint(10, 110),
+                avg_non_commute_distance=random.randint(10,20),
+                battery_size_kwh=random.randint(50,100),
+                min_charge_kwh=0.0
+            )
+            # Set min charge based on maximum expected distance
+            max_distance = max(ev.avg_commute_distance, ev.avg_non_commute_distance)
+            ev.min_charge_kwh = max_distance * ev_consumption + ev.battery_size_kwh * 0.2
+
+            ev_path = f"./data/ev/test/{idx}.csv"
+            wfh_days = random.sample([0,1,2,3,4], 5-ev.num_commute_trips)
+
+            schedule = [0 for _ in range(5)]
+            for wfh_day in wfh_days:
+                schedule[wfh_day] = 1
+            ev_trace = f"python ./data/ev/ev_simulation.py --output {ev_path} --days 365 --ev_battery {ev.battery_size_kwh} --max_soc 0.8 --min_soc 0.2 --consumption 164 --wfh_monday {schedule[0]} --wfh_tuesday {schedule[1]} --wfh_wednesday {schedule[2]} --wfh_thursday {schedule[3]}  --wfh_friday {schedule[4]} --C_dist {ev.avg_commute_distance} --C_dept 8.00 --C_arr 18.00 --N_nc {ev.num_non_commute_trips} --Nc_dist {ev.avg_non_commute_distance}"
+            _ = subprocess.run(ev_trace.split(), stdout=subprocess.PIPE, text=True)
+
+            for op in policies:
+                tasks.append((ev_path, solar_file, load_file, op, ev, "test"))
 
         with multiprocessing.Pool(processes=num_processes) as pool:
-            results = pool.map(process_pair_test, tasks)
+            results = pool.map(process_pair, tasks)
 
         print(f"Round {round_num + 1} completed.")
-
-    print("All rounds completed.")
